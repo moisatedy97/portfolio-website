dcf74bc0321802f597083726f6d545091d960170
Tedy Gabriel Moisa <moisatedy@gmail.com>
2023-11-16 23:32:09 +0100

    Supabase working

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,29 +1,15 @@
 import DeployButton from "../components/DeployButton";
 import AuthButton from "../components/AuthButton";
-import { createClient } from "@/utils/supabase/server";
 import ConnectSupabaseSteps from "@/components/ConnectSupabaseSteps";
 import SignUpUserSteps from "@/components/SignUpUserSteps";
 import Header from "@/components/Header";
-import { cookies } from "next/headers";
+import { isSupabaseConnected, supabase } from "@/utils/supabase/init";
 
 export default async function Index() {
-    const cookieStore = cookies();
-
-    const canInitSupabaseClient = () => {
-        // This function is just for the interactive tutorial.
-        // Feel free to remove it once you have Supabase connected.
-        try {
-            createClient(cookieStore);
-            return true;
-        } catch (e) {
-            return false;
-        }
-    };
-
-    const isSupabaseConnected = canInitSupabaseClient();
+    const { data: notes } = await supabase.from("notes").select();
 
     return (
-        <div className="flex w-full flex-1 flex-col items-center gap-20">
+        <div>
             <nav className="border-b-foreground/10 flex h-16 w-full justify-center border-b">
                 <div className="flex w-full max-w-4xl items-center justify-between p-3 text-sm">
                     <DeployButton />
@@ -38,20 +24,7 @@ export default async function Index() {
                     {isSupabaseConnected ? <SignUpUserSteps /> : <ConnectSupabaseSteps />}
                 </main>
             </div>
-
-            <footer className="border-t-foreground/10 flex w-full justify-center border-t p-8 text-center text-xs">
-                <p>
-                    Powered by{" "}
-                    <a
-                        href="https://supabase.com/?utm_source=create-next-app&utm_medium=template&utm_term=nextjs"
-                        target="_blank"
-                        className="font-bold hover:underline"
-                        rel="noreferrer"
-                    >
-                        Supabase
-                    </a>
-                </p>
-            </footer>
+            <pre>{JSON.stringify(notes, null, 2)}</pre>
         </div>
     );
 }
