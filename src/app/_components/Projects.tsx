a5d4684aaee3a2d99b2d3cdfbcb043865a0dfb4f
gmoisa <gabriel.moisa@wareplace.com>
2023-12-27 09:31:23 +0100

    projects structure init

--- src/app/_components/Projects.tsx
+++ src/app/_components/Projects.tsx
@@ -1,7 +1,10 @@
+import CardCarousel from "@/components/CardCarousel";
 import { Project } from "@/interfaces/Projects";
 import supabaseServer from "@/supabase/config";
 import { QueryData, QueryError } from "@supabase/supabase-js";
-import { ReactElement } from "react";
+import { ReactElement, ReactNode } from "react";
+import Image from "next/image";
+import { Card, CardContent, CardDescription, CardFooter, CardHeader, CardTitle } from "@/components/ui/card";
 
 export default async function Projects(): Promise<ReactElement | undefined> {
   const query = supabaseServer().from("projects").select("*, frameworks (*)").eq("id", 1);
@@ -13,7 +16,8 @@ export default async function Projects(): Promise<ReactElement | undefined> {
 
   if (data) {
     return (
-      <div className="w-full">
+      <div className="flex w-full flex-col gap-4 px-[2rem] py-[6rem] md:px-[8rem] md:py-[8rem] lg:px-[20rem] lg:py-[18rem]">
+        <h3 className="text-xl font-bold text-primary md:text-2xl">Projects</h3>
         {data.map((project: Project) => {
           return <Project project={project} />;
         })}
@@ -27,5 +31,39 @@ type ProjectProps = {
 };
 
 const Project = ({ project }: ProjectProps): ReactElement => {
-  return <div></div>;
+  const createImageCards = (project: Project): ReactNode[] => {
+    const imageCards: ReactNode[] = [];
+
+    project.images.forEach((image: string) => {
+      imageCards.push(
+        <div className="relative h-60 w-48">
+          <Image
+            className="origin-center rounded-lg shadow-sm shadow-primary"
+            priority={true}
+            fill={true}
+            sizes="100vw, 100vh"
+            src={image}
+            alt={project.name}
+          />
+        </div>,
+      );
+    });
+
+    return imageCards;
+  };
+
+  return (
+    <div className="flex bg-primary-foreground p-4">
+      <CardCarousel cards={createImageCards(project)} />
+      <Card className="flex flex-col justify-between shadow-sm shadow-primary dark:bg-black">
+        <CardHeader className="px-6 py-4">
+          <CardTitle className="text-lg md:text-2xl">{project.name}</CardTitle>
+        </CardHeader>
+        <CardContent className="overflow-auto">
+          <CardDescription className="text-[0.7em] md:text-xs">{project.description}</CardDescription>
+        </CardContent>
+        <CardFooter className="self-end py-4 text-sm text-primary/90 md:text-lg">{project.host}</CardFooter>
+      </Card>
+    </div>
+  );
 };
