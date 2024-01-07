15c06a2e2b31a3c0f4f5fc6bac30e5095d1d99f1
Tedy Gabriel Moisa <moisatedy@gmail.com>
2024-01-07 13:29:41 +0100

    Removed netlify crontab, added vercel crontab

--- src/app/api/crontab/route.ts
+++ src/app/api/crontab/route.ts
@@ -1,9 +1,9 @@
-import { EmailOptions, emailOptionsSchema } from "@/interfaces/EmailOptions";
 import { mailTransporter } from "@/mailer";
 import supabaseServer from "@/supabase/config";
 import { QueryData, QueryError } from "@supabase/supabase-js";
 
 export async function GET() {
+  const isEmailActive: string | undefined = process.env.NEXT_EMAIL;
   const query = supabaseServer().from("profile").select("*").eq("id", 1);
   const { data, error }: { data: QueryData<typeof query> | null; error: QueryError | null } = await query;
 
@@ -11,16 +11,19 @@ export async function GET() {
     throw error;
   }
 
-  if (data && data[0].id > 0) {
-    try {
-      await mailTransporter.sendMail({
-        to: data[0].email,
-        subject: "Portfolio website reset",
-        text: "Supabase request was sent. Supabase was restarted.",
-      });
-      return new Response("Email send", { status: 200 });
-    } catch (error) {
-      return new Response("Send email error", { status: 400 });
+  if (isEmailActive && isEmailActive === "true") {
+    if (data && data[0].id > 0) {
+      try {
+        await mailTransporter.sendMail({
+          to: data[0].email,
+          subject: "Portfolio website reset",
+          text: "Supabase request was sent. Supabase was restarted.",
+        });
+
+        return new Response("Email send", { status: 200 });
+      } catch (error) {
+        return new Response("Send email error", { status: 400 });
+      }
     }
   }
 
